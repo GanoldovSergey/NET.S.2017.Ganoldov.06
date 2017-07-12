2de68d8c3ded6072dce88851e8ffa561000fa4e6
dolg6 <dolg6@DESKTOP-MEAMC1D>
2017-07-12 18:25:14 +0300

    2

---
+++ Logic.Tests/SortTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Logic.Tests
+{
+    public class SortTest
+    {
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_IncreaseSum()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            IncreaseSum comparer = new IncreaseSum();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_DecreaseSum()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            DecreaseSum comparer = new DecreaseSum();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_IncreaseMax()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            IncreaseMax comparer = new IncreaseMax();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_DecreaseMax()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            DecreaseMax comparer = new DecreaseMax();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_IncreaseMin()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            IncreaseMin comparer = new IncreaseMin();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public int[][] Sort_DecreaseMin()
+        {
+            int[][] arr = { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4, 5 } };
+            DecreaseMin comparer = new DecreaseMin();
+            BubbleSort.Sort(arr, comparer);
+            return arr;
+        }
+
+
+        private class IncreaseSum : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => lhs.Sum() - rhs.Sum();
+        }
+        private class DecreaseSum : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => rhs.Sum() - lhs.Sum();
+        }
+        private class IncreaseMax : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => lhs.Max() - rhs.Max();
+        }
+        private class DecreaseMax : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => rhs.Max() - lhs.Max();
+        }
+        private class IncreaseMin : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => lhs.Min() - rhs.Min();
+        }
+        private class DecreaseMin : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs) => rhs.Min() - lhs.Min();
+        }
+    }
+}
---
+++ Logic/BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class BubbleSort
+    {
+        /// <summary>
+        /// Method for sorting jugged arrays
+        /// </summary>
+        /// <param name="array">Array to be sorted </param>
+        /// <param name="comparer"></param>
+        public static void Sort(int[][] array, IComparer<int[]> comparer)
+        {
+            CheckValidArray(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[i], array[j]) > 0)
+                        Swap(ref array[i], ref array[j]);
+                }
+            }
+        }
+
+        private static void Swap(ref int[] lhs, ref int[] rhs)
+        {
+            int[] temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private static void CheckValidArray(int[][] array)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException($"{nameof(array)} is null");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (ReferenceEquals(array[i], null))
+                    throw new ArgumentNullException($"{nameof(array)}[{i}] is null");
+            }
+        }
+    }
+}
